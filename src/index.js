6947274c354dcf93c078794569428c1aa1d8d328
SindreH <sindre.hepsoe@gmail.com>
2020-06-08 23:12:20 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'tachyons'
+import App from './containers/app'
+
+ReactDOM.render(
+    <App />
+  ,
+  document.getElementById('root')
+);
